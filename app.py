5ab0847a4a317dc21fa5896fa64b9558fac13ca4
jjkim110523 <jjkim110523@gmail.com>
2022-06-28 14:49:44 +0900

    06/28/2022

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 # dictionary-api-python-flask/app.py
 from flask import Flask, request, jsonify, render_template
+from itsdangerous import json
+from pyparsing import Word
 from model.dbHandler import match_exact, match_like
 
 app = Flask(__name__)
@@ -7,26 +9,32 @@ app = Flask(__name__)
 
 @app.get("/")
 def index():
-    """
-    DEFAULT ROUTE
-    This method will
-    1. Provide usage instructions formatted as JSON
-    """
-    # response = {"usage": "/dict?=<word>"}
-    # Since this is a website with front-end, we don't need to send the usage instructions
-    return "TODO"
+    response = {"usage": "/dict?=<word>"}
+    return jsonify(response)
 
 
 @app.get("/dict")
 def dictionary():
-    """
-    DEFAULT ROUTE
-    This method will
-    1. Accept a word from the request
-    2. Try to find an exact match, and return it if found
-    3. If not found, find all approximate matches and return
-    """
-    return "TODO"
+    words = request.args.getlist("word")    
+    
+    if not words:
+        response = {"status": "error", "word": words, "data": "word not found"}
+        return jsonify(response)
+    
+    response = {"words": []}
+
+    for word in words:
+        definitions = match_exact(word)
+        if definitions:
+            response["words"].append({"status": "sucess", "word": word, "data": definitions})
+        else:
+            definitions = match_like(word)
+            if definitions:
+                response["words"].append({"status": "partial", "word": word, "data": definitions})
+            else:
+                response["words"].append({"status": "error", "word": word, "data": "word not found"})
+    return jsonify(response)
+
 
 
 if __name__ == "__main__":
